dda49a9a3c948da68c99c92853a5d2fec4782062
41KailasRakh <kailasrakh@gmail.com>
2025-02-14 13:51:31 +0530

    Project setup inital coomit

---
+++ src/test/java/org/paygateway/PaygatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.paygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaygatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
